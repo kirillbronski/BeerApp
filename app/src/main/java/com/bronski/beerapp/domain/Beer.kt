8b65faa32be7bbbf22065cbec6cfe559eb99c2da
Kirill <kirillbronski@gmail.com>
2023-04-14 00:23:02 +0300

    implemented api and remote mediator

---
+++ app/src/main/java/com/bronski/beerapp/domain/Beer.kt
@@ -0,0 +1,10 @@
+package com.bronski.beerapp.domain
+
+data class Beer(
+    val id: Int,
+    val name: String,
+    val tagline: String,
+    val description: String,
+    val firstBrewed: String,
+    val imageUrl: String?
+)
