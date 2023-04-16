7b557168d91e4e416bbcaec37613ac5e97d7747e
Kirill <kirillbronski@gmail.com>
2023-04-16 20:49:48 +0300

    implemented BeerItem - WIP 30:40

---
+++ app/src/main/java/com/bronski/beerapp/presentation/BeerScreen.kt
@@ -0,0 +1,27 @@
+package com.bronski.beerapp.presentation
+
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import com.bronski.beerapp.domain.Beer
+
+@Composable
+fun BeerScreen(
+    beers: LazyPagingItems<Beer>
+) {
+
+    val context = LocalContext.current
+    LaunchedEffect(key1 = beers.loadState) {
+        if (beers.loadState.refresh is LoadState.Error) {
+            Toast.makeText(
+                context,
+                "Error: " + (beers.loadState.refresh as LoadState.Error).error.message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+}
